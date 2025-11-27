5aacce28be6adc7e5b1ccaa2973ede91a881538c
Tran Trung Chien k17 HL <chientthe170152@fpt.edu.vn>
2025-11-27 08:05:31 +0700

    them 1 atttribute o class Test3

--- NewDirectory1/Test3.cs
+++ NewDirectory1/Test3.cs
@@ -5,7 +5,13 @@ public class Test3
     private String abc;
 
     private String nguvl;
-    
+
+    private String ocCut;
+
     public Test3()
-    {}
+    {
+        abc = "abc";
+        nguvl = "nguvl";
+        ocCut = "oc-cut dell bt git hub";
+    }
 }
