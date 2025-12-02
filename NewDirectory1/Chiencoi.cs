300efce822c6a1ec5a64138bd1117eef23138d90
Tran Trung Chien k17 HL <chientthe170152@fpt.edu.vn>
2025-12-02 19:52:24 +0700

    Save Chiencoi.cs before switching branch

---
+++ NewDirectory1/Chiencoi.cs
@@ -0,0 +1,15 @@
+namespace DefaultNamespace;
+
+public class Chiencoi
+{
+    private string chiencoi;
+    private string fullName;
+    
+
+    public Chiencoi()
+    {
+        chiencoi = "Chiencoi";
+        fullName = "Chiencoi";
+        
+    }
+}
