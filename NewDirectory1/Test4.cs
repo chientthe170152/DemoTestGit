3d9dca00b7f48e497596bd3b7df1dbffd15369da
Tran Trung Chien k17 HL <chientthe170152@fpt.edu.vn>
2025-12-27 18:31:53 +0700

    Merger class Test4 vao nhanh Dev

---
+++ NewDirectory1/Test4.cs
@@ -0,0 +1,14 @@
+namespace DefaultNamespace;
+
+public class Test4
+{
+    private int abc;
+    private string bcd;
+    private double b;
+    private double d;
+    
+    public class MyClass
+    {
+        private int abc;
+    }
+}
